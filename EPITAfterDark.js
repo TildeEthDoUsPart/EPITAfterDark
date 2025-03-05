363df9fbcbd8aa4089f48523d7033b2302bc7a86
TildeEthDoUsPart <40714786+TildeEthDoUsPart@users.noreply.github.com>
2025-03-05 10:38:03 +0100

    now its a userscript

--- EPITAfterDark.js
+++ EPITAfterDark.js
@@ -1,3 +1,14 @@
+// ==UserScript==
+// @name         EPTAfterDark
+// @namespace    http://tampermonkey.net/
+// @version      2025-03-05
+// @description  Dark Mode for EPITA's moodle. Feel free to contribute!
+// @author       TildeEth
+// @match        https://moodle.epita.fr/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=epita.fr
+// @grant        none
+// ==/UserScript==
+
 function addStyleSheet(style){
   var getHead = document.getElementsByTagName("HEAD")[0];
   var cssNode = window.document.createElement( 'style' );
